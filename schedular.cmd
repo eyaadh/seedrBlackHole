3cadd3e4ad84e2fcce2ecaba9a437eb6db09a7b0
eyaadh <eyaadh@gmail.com>
2020-12-27 04:37:06 +0600

    added a small batch command for scheduler to start the app automatically at login

---
+++ schedular.cmd
@@ -0,0 +1,4 @@
+C:
+REM change the following path on change directory to where you have cloned the repo:
+cd C:\\Users\\Administrator\\Documents\\seedrBlackHole
+start "" "%PROGRAMFILES%\Git\bin\\sh.exe" -c "python -m bh"
